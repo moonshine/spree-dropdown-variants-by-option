d23f0e028dad7102c1ecab6ba3dba5ae653a1bf3
Etienne van Tonder <etiennevt@gmail.com>
2010-06-22 08:36:54 +1000

    Display out of stock message

--- lib/spree/dropdown_variants_by_option/products_controller.rb
+++ lib/spree/dropdown_variants_by_option/products_controller.rb
@@ -13,8 +13,9 @@ module Spree::DropdownVariantsByOption::ProductsController
     render :update do |page|
       @product = Product.find_by_permalink(params[:id])
       @selected_variant = Variant.find_by_option_values(@product.id, params[:option_values])
-        @variants = [@selected_variant]
-        page.replace_html 'thumbnails', :partial => 'thumbnails', :locals => {:product => @product}
+      @variants = Array.new
+      @variants << @selected_variant if @selected_variant
+      page.replace_html 'thumbnails', :partial => 'thumbnails', :locals => {:product => @product}
       unless @selected_variant
         page.replace_html 'variant-images', :text => "#{t('out_of_stock')}"
       end
