660da9fb4094c5b259ec955f0f89e3d81e74c223
Etienne van Tonder <etiennevt@gmail.com>
2010-06-23 18:11:13 +1000

    Display errors when using ajax add to cart

--- lib/spree/dropdown_variants_by_option/orders_controller.rb
+++ lib/spree/dropdown_variants_by_option/orders_controller.rb
@@ -1,9 +1,11 @@
 module Spree::DropdownVariantsByOption::OrdersController
+  JS_ESCAPE_MAP = { '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" }
 
   def self.included(target)
     target.class_eval do
       create.before << :add_variants_from_option_values
       create.wants.js
+      create.failure.wants.js { render :js => "alert('#{format_js_error}')" }
     end
   end
 
@@ -11,10 +13,17 @@ module Spree::DropdownVariantsByOption::OrdersController
 
   def add_variants_from_option_values
     if params[:option_values] and params[:product_id]
-      variant = Variant.find_by_option_values(params[:product_id], params[:option_values])
+      @variant = Variant.find_by_option_values(params[:product_id], params[:option_values])
       quantity = params[:quantity].to_i
-      @object.dropdown_variants_by_option_add_variant(variant, quantity) if quantity > 0
+      @object.dropdown_variants_by_option_add_variant(@variant, quantity) if quantity > 0
     end
+  end
+
+  def format_js_error
+    message = "The following error(s) have occurred when trying to add the item to the cart: "
+    @order.errors.each_full {|m| message += "#{m} "}
+    message.gsub(/(\\|<\/|\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] }
+    message
   end
 
 end
--- public/javascripts/dropdown_variants_by_option.js
+++ public/javascripts/dropdown_variants_by_option.js
@@ -2,22 +2,56 @@ jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function(e) {
-    e.preventDefault();
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
+jQuery.fn.extend
+(
+  {
+    submitWithAjax: function()
+    {
+      this.submit
+      (
+        function(e)
+        {
+          e.preventDefault();
+          $.post(this.action, $(this).serialize(), null, "script");
+          return false;
+        }
+      )
+      return this;
+    }
+  }
+);
 
 
-$(document).ready(function() {
-  $("#cart_form").submitWithAjax();
-  $('body').ajaxStart(function () {
-    $("#busy_indicator").show();
-  });
-  $('body').ajaxStop(function () {
-    $("#busy_indicator").hide();
-  });
-})
+var app =
+{
+  setupAjaxCallbacks: function ()
+  {
+    $('body').ajaxStart(
+      function ()
+      {
+        $("#busy_indicator").show();
+      }
+    );
+    $('body').ajaxStop(
+      function ()
+      {
+        $("#busy_indicator").hide();
+      }
+    );
+  },
+
+  addToCart: function()
+  {
+    $("#cart_form").submitWithAjax();
+  }
+}
+
+jQuery
+(
+  function ()
+  {
+    app.setupAjaxCallbacks();
+
+    app.addToCart();
+  }
+);
