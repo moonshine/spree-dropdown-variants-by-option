a54e9cad87edcb855b4d4993f0f251ec00ba03aa
Etienne van Tonder <etiennevt@gmail.com>
2010-07-02 14:01:38 +1000

    Clear flash on add to cart, cleanup js code

--- public/javascripts/dropdown_variants_by_option.js
+++ public/javascripts/dropdown_variants_by_option.js
@@ -2,80 +2,52 @@ jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
-jQuery.fn.extend
-(
-  {
-    submitWithAjax: function()
-    {
-      this.submit
-      (
-        function(e)
-        {
-          e.preventDefault();
-          $.post(this.action, $(this).serialize(), null, "script");
-          return false;
-        }
-      )
+jQuery.fn.extend( {
+  submitWithAjax: function() {
+    this.submit(function(e) {
+      e.preventDefault();
+      $.post(this.action, $(this).serialize(), null, "script");
+      return false;
+      })
       return this;
     },
 
-    onChangeWithAjax: function()
-    {
-      this.change
-      (
-        function(e)
-        {
-          e.preventDefault();
-          $.post('/products/option_value_changed', $("#cart_form").serialize(), null, "script");
-          return false;
-        }
-      )
+    onChangeWithAjax: function() {
+      this.change(function(e) {
+        e.preventDefault();
+        $.post('/products/option_value_changed', $("#cart_form").serialize(), null, "script");
+        return false;
+      })
       return this;
     }
-
-
-  }
-);
+});
 
 
 var app =
 {
-  setupAjaxCallbacks: function ()
-  {
-    $('body').ajaxStart(
-      function ()
-      {
-        $("#busy_indicator").show();
-      }
-    );
-    $('body').ajaxStop(
-      function ()
-      {
-        $("#busy_indicator").hide();
-      }
-    );
+  setupAjaxCallbacks: function () {
+    $('body').ajaxStart(function (){
+      $("#busy_indicator").show();
+    });
+    $('body').ajaxStop(function () {
+      $("#busy_indicator").hide();
+    });
   },
 
-  addToCart: function()
-  {
+  addToCart: function() {
     $("#cart_form").submitWithAjax();
   },
 
-  optionValueChanged: function()
-  {
+  optionValueChanged: function() {
     $("select#option_values_select").onChangeWithAjax();
     $("input#option_values_radio").onChangeWithAjax();
   }
 }
 
-jQuery
-(
-  function ()
-  {
-    app.setupAjaxCallbacks();
+jQuery( function () {
+  app.setupAjaxCallbacks();
 
-    app.addToCart();
+  app.addToCart();
 
-    app.optionValueChanged();
-  }
-);
+  app.optionValueChanged();
+});
