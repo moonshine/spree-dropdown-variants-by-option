2dae928251247d1d430ab8bac1b43aac1ea1c2e7
Trung Lê <joneslee85@gmail.com>
2010-09-17 10:40:43 +1000

    Simplify SQL way with AR way

--- lib/spree/dropdown_variants_by_option/variant.rb
+++ lib/spree/dropdown_variants_by_option/variant.rb
@@ -2,18 +2,14 @@ module Spree::DropdownVariantsByOption::Variant
 
   def self.included(target)
     target.class_eval do
-      # Try and find the variant from the supplied option values and product
-      def self.find_by_option_values(product, option_values)
-        join_clause = ''
-        and_clause = ''
-        option_values.each_with_index do |option_value, i|
-          join_clause << " INNER JOIN option_values_variants ovv#{i} ON variants.id = ovv#{i}.variant_id "
-          and_clause << " AND ovv#{i}.option_value_id = #{option_value.last} "
+      # Try and find the variant from the supplied pair option_type <--> option value and product
+      def self.find_by_option_values(product_id, opt_values)
+        variants = Array.new
+        product = Product.find(product_id)
+        opt_values.each_pair do |opt_type, opt_value|
+          variants << product.variants.select { |v| v.option_values.include? OptionValue.find_by_option_type_id_and_id(opt_type, opt_value) }
         end
-        Variant.find_by_sql("SELECT variants.* FROM variants #{join_clause}
-          WHERE variants.deleted_at is null AND
-          variants.product_id = #{product}
-          #{and_clause};")
+        variants.flatten
       end
     end
   end
